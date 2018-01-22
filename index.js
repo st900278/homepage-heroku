aa1ca3336500a998bb99181be33aea47959958fd
oberonfrog <oberonfrog@gmail.com>
2018-01-23 00:16:56 +0900

    add semantic

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-var express = require('express')
+var express = require('express');
+var mustache = require("mustache-express");
 var app = express()
 
+app.set('port', (process.env.PORT || 8000));
+app.engine('html', mustache());
+app.set('view engine', 'html');
+app.set("views", __dirname + '/views');
+
+app.use(express.static('semantic'));
 // GET method route
-app.get('/', function (req, res) {
-  res.send('GET request to the homepage')
+app.get('/', function(req, res) {
+    res.render('index');
 })
 
 // POST method route
-app.post('/', function (req, res) {
-  res.send('POST request to the homepage')
+app.post('/', function(req, res) {
+    res.send('POST request to the homepage')
 })
 
-app.set('port', (process.env.PORT || 8000));
 
-app.listen(app.get('port'), () => console.log('Example app listening on port 3000!'))
+app.listen(app.get('port'), () => console.log('Listening on port ' + app.get('port')))
